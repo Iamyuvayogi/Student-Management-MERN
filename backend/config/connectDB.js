cf4e7e21d03f5fd6321219e0e26ae77b5880cddc
Iamyuvayogi <mryuvayogi@gmail.com>
2024-03-21 20:38:35 +0530

    removed colors npm

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-const colors = require("colors");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
-    console.log(`MongoDB Server Run On ${mongoose.connection.host}`.bgGreen);
+    console.log(`MongoDB Server Run On ${mongoose.connection.host}`);
   } catch (error) {
-    console.log(`${error}`.bgRed);
+    console.log(`${error}`);
   }
 };
 module.exports = connectDB;
